10d2959648476efc237bb14d86efa72682c68a8b
Leticia <leticiavidal2910@gmail.com>
2024-12-23 13:02:15 -0300

    fix: adjusted listen method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js"; //need to use the .js in the end because of the "type": "module" in package.json config
 
 const app = express();
 
 app.use("/api/auth", authRoutes)
 
-app.listeners(5001, () => {
+app.listen(5001, () => {
     console.log("server is running on port 5001")
 });
