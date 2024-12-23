3d02d2cb7ba6f434cc28abb42231c4b50735c798
Leticia <leticiavidal2910@gmail.com>
2024-12-23 16:02:42 -0300

    feat: added parser to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js"; //need to use the .js in the end because of the "type": "module" in package.json config
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config()
 
@@ -9,7 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.use(express.json()) //allow to extract the json data aout of the bodyrequest
+app.use(express.json()); //allow to extract the json data aout of the bodyrequest
+app.use(cookieParser()); // allow to parse the cookie and grab the values out of it
 
 app.use("/api/auth", authRoutes)
 
