15257c1bacedfbfd8f8067b6bd62f1d0c478e5f1
Leticia <leticiavidal2910@gmail.com>
2024-12-23 16:05:40 -0300

    feat: added update profile pic route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { login, signup,logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post("/signup", signup)
 router.post("/login", login)
 
 router.post("/logout", logout)
+
+router.purge("/update-profile", protectRoute, updateProfile)
 
 export default router;
