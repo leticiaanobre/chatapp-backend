d8f87b47b8b426e98dcfb3a3aed686e4d36b0590
Leticia <leticiavidal2910@gmail.com>
2024-12-23 15:14:30 -0300

    refat: written logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -78,7 +78,7 @@ export const login = async (req, res) => {
 export const logout =  (req, res) => {
     try {
         res.cookie("jwt", "", {maxAge:0}) //clean token
-        res.status(200).json({ message: "Loggout successfully"});
+        res.status(200).json({ message: "Logout successfully"});
     } catch (error) {
         console.log("Error in logout controller", error.message);
         res.status(500).json({ message: "Internal Server Error"});
