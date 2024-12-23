f827cbafceca51d6a04c311e5863b119f90c2800
Leticia <leticiavidal2910@gmail.com>
2024-12-23 14:38:18 -0300

    feat: finished newUser controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import generateToken from "../lib/utils.js"
 
 export const signup =  async (req, res) => {
     const{fullName, email, password} = req.body
@@ -23,11 +24,21 @@ export const signup =  async (req, res) => {
 
         if(newUser) {
             //generate jwt token
+            generateToken(newUser._id,res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic,
+            });
         } else {
             res.status(400).json({message: "Invalid user data"})
         }
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error"});
     }
 }
 export const login =  (req, res) => {
