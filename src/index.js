fe4d9fa3dad69ee3bb3e7dc09aee42f428bf1c99
Leticia <leticiavidal2910@gmail.com>
2024-12-23 13:56:43 -0300

    feat: connection to db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js"; //need to use the .js in the end because of the "type": "module" in package.json config
 import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config()
 
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT)
+    connectDB()
 });
