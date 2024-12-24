b4ebf54e1c12784eaaddb57e9f1f109ad44a1bda
Leticia <leticiavidal2910@gmail.com>
2024-12-23 21:44:00 -0300

    fix: unwritten code

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const protectRoute = async (req, resizeBy, next) => {
+export const protectRoute = async (req, res, next) => {
     try {
-        const token = req.cookie.jwt
+        const token = req.cookies.jwt
 
         if(!token) {
             return res.status(401).json({message: "Unauthorized - No token provided"});
