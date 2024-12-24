c92a081cb23530ca7243dbc2437c8334c56d9739
Leticia <leticiavidal2910@gmail.com>
2024-12-23 22:00:10 -0300

    feat: added message routes from users

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js"; //need to use the .js in the end because of the "type": "module" in package.json config
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -13,7 +14,8 @@ const PORT = process.env.PORT
 app.use(express.json()); //allow to extract the json data aout of the bodyrequest
 app.use(cookieParser()); // allow to parse the cookie and grab the values out of it
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT)
