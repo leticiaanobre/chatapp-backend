d549491376df009256a06e88fbd383945d606419
Leticia <leticiavidal2910@gmail.com>
2024-12-23 21:30:41 -0300

    feat: added updated profile controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import generateToken from "../lib/utils.js"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup =  async (req, res) => {
     const{fullName, email, password} = req.body
@@ -81,6 +82,25 @@ export const logout =  (req, res) => {
         res.status(200).json({ message: "Logout successfully"});
     } catch (error) {
         console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error"});
+    }
+}
+
+export const updateProfile = async(req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({ message: "Profile pic is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new:true})
+
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log("Error in update profile:", error.message);
         res.status(500).json({ message: "Internal Server Error"});
     }
 }
