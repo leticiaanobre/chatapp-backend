7ca9127279a6ca86fb804c39352a194129866e5a
Leticia <leticiavidal2910@gmail.com>
2024-12-23 22:07:22 -0300

    feat: created getMessages controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async(req,res) => {
@@ -7,7 +8,26 @@ export const getUsersForSidebar = async(req,res) => {
 
         res.status(200).json(filteredUsers)
     } catch (error) {
-        console.log("Error in getUsersForSidebar: ", error.message);
+        console.log("Error in getUsersForSidebar controller: ", error.message);
+        res.status(500).json({ error: "Internal server error"});
+    }
+}
+
+export const getMessages = async (req,res) => {
+    try {
+        const {id: userToChatId} = req.params
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: senderId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId: myId}
+            ]
+        })
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
         res.status(500).json({ error: "Internal server error"});
     }
 }
