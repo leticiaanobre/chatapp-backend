310aff63bfe083e8674a36316d115a637d9a9c84
Leticia <leticiavidal2910@gmail.com>
2024-12-28 15:32:25 -0300

    feat: created socket io app and server and replace it in index

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config()
 
-const app = express();
+// const app = express(); {dont need to use anymore because we created a new one with socket io in socket.js}
 
 const PORT = process.env.PORT
 
@@ -22,7 +23,12 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//     console.log("server is running on PORT:" + PORT)
+//     connectDB()
+// }); REPLACE THE APP WITH SERVER CREATED IN SOCKET.JS
+
+server.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT)
     connectDB()
 });
